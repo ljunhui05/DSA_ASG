e41257e2344b80b046f8aedc807d3b1e3407da7a
limjunhui <110986279+ljunhui05@users.noreply.github.com>
2023-08-07 23:54:29 +0800

    Add initialise data function

--- DSA_ASG/DSA_ASG.cpp
+++ DSA_ASG/DSA_ASG.cpp
@@ -1,9 +1,61 @@
 // DSA_ASG.cpp : This file contains the 'main' function. Program execution begins and ends there.
 //
-
 #include <string>
 #include <iostream>
 using namespace std;
+
+#include "Admin.h";
+#include"Dictionary.h";
+#include"Food.h";
+#include"HashTable.h";
+#include"List.h";
+#include"Member.h"
+#include"Order.h";
+#include"OrderQueue.h";
+
+
+void initData() 
+{
+    Dictionary foodMenu;
+    List<Admin>adminList;
+    HashTable memberHashTable;
+
+    Member Member1("JunHui", "passJunHui", 100);
+    Member Member2("Ervin", "passErvin", 50);
+    Member Member3("Keene", "passKeene", 1000 );
+
+    memberHashTable.add(Member1.getName(), Member1);
+    memberHashTable.add(Member2.getName(), Member2);
+    memberHashTable.add(Member3.getName(), Member3); 
+
+    Admin Admin1("Admin1", "passAdmin1");
+
+    adminList.add(Admin1);
+
+    Food Food1(1, "Pizza", "Western", "Bishan", 12, 10);
+    Food Food2(2, "Hot Dogs", "Western", "Bishan", 5, 50);
+    Food Food3(3, "Carbonara", "Western", "Bishan", 7, 25);
+    Food Food4(6, "Prawn Noodles", "Chinese", "Bishan", 7, 25);
+    Food Food5(7, "Dim Sum", "Chinese", "Bishan", 8, 30);
+
+    foodMenu.add("PizzaBishan", Food1);
+    foodMenu.add("HotDogsBishan", Food2);
+    foodMenu.add("CarbonaraBishan", Food3);
+    foodMenu.add("PrawnNoodles", Food4);
+    foodMenu.add("DimSum", Food5);
+
+    Food Food6(1, "Pizza", "Western", "Punggol", 12, 10);
+    Food Food7(2, "Hot Dogs", "Western", "Punggol", 5, 50);
+    Food Food8(3, "Carbonara", "Western", "Punggol", 7, 25);
+    Food Food9(4, "Hamburger", "Western", "Punggol", 8, 30);
+    Food Food10(5, "Creme Brulee", "Western", "Punggol", 11, 15);
+
+    foodMenu.add("PizzaPunggol", Food6);
+    foodMenu.add("HotDogsPunggol", Food7);
+    foodMenu.add("CarbonaraPunggol", Food8);
+    foodMenu.add("Hamburger", Food9);
+    foodMenu.add("CremeBrulee", Food10);
+}
 
 void printMainMenu() {
     cout << "+----------------------------+" << endl;
@@ -22,20 +74,17 @@ void printMainMenu() {
 void printRegisterMemberHeader(){
     cout << "+---------------------------------+" << endl;
     cout << "+       Register as a Member      +" << endl;
-    cout << "+  (Enter 'x' to return to menu)  +" << endl;
     cout << "+---------------------------------+" << endl;
 }
 void printAdminLoginHeader() {
     cout << "+---------------------------------+" << endl;
     cout << "+         Login as Admin          +" << endl;
-    cout << "+  (Enter 'x' to return to menu)  +" << endl;
     cout << "+---------------------------------+" << endl;
 }
 
 void printMemberLoginHeader() {
     cout << "+---------------------------------+" << endl;
     cout << "+        Login as Member          +" << endl;
-    cout << "+  (Enter 'x' to return to menu)  +" << endl;
     cout << "+---------------------------------+" << endl;
 }
 
@@ -44,19 +93,14 @@ void registerMember() {
     string memberPassword = "";
     cout << "Please enter an email: " << endl;
     cin >> memberEmail;
-    if (memberEmail == "x") {
-        main();
-    }
+
     cout << "Please enter a password: " << endl;
     cin >> memberPassword;
-    if (memberPassword == "x") {
-        main();
-    }
-
 
 }
 
 int main(){
+    initData();
     printMainMenu();
     int opt = 0;
     cin >> opt;
@@ -64,7 +108,7 @@ int main(){
     if (opt == 1) 
     {
         printRegisterMemberHeader();
-
+        registerMember();
     }
 
     else if (opt == 2) 
