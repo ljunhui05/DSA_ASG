1cc641712821962d4b129ecba3d9300b55b69c86
limjunhui <110986279+ljunhui05@users.noreply.github.com>
2023-08-08 21:06:58 +0800

    Add File I/O for Member Details, printFoodMenu, registerMember, memberLogin and mainMenu functions

--- DSA_ASG/DSA_ASG.cpp
+++ DSA_ASG/DSA_ASG.cpp
@@ -1,7 +1,10 @@
 // DSA_ASG.cpp : This file contains the 'main' function. Program execution begins and ends there.
 //
-#include <string>
 #include <iostream>
+#include <string>
+#include<fstream>
+#include<sstream>
+#include <iomanip>
 using namespace std;
 
 #include "Admin.h";
@@ -14,19 +17,36 @@ using namespace std;
 #include"OrderQueue.h";
 
 
+Dictionary foodMenu;
+List<Admin>adminList;
+HashTable memberHashTable;
+Member loggedInMember;
+Queue orderQueue;
+
+void initCustomer() {
+
+    ifstream inFile("Members.csv");
+    if (!inFile) {
+        cerr << "Error opening Members.csv for reading." << std::endl;
+        return;
+    }
+
+    string line;
+
+    while (getline(inFile, line)) {
+        istringstream iss(line);
+        string name, password, money, points;
+        if (getline(iss, name, ',') && getline(iss, password, ',') && getline(iss, money, ',') && getline(iss, points)) {
+            Member member (name, password, stod(money), stoi(points));
+            memberHashTable.add(name, member);
+        }
+    }
+
+}
+
 void initData() 
 {
-    Dictionary foodMenu;
-    List<Admin>adminList;
-    HashTable memberHashTable;
-
-    Member Member1("JunHui", "passJunHui", 100);
-    Member Member2("Ervin", "passErvin", 50);
-    Member Member3("Keene", "passKeene", 1000 );
-
-    memberHashTable.add(Member1.getName(), Member1);
-    memberHashTable.add(Member2.getName(), Member2);
-    memberHashTable.add(Member3.getName(), Member3); 
+    initCustomer();
 
     Admin Admin1("Admin1", "passAdmin1");
 
@@ -57,74 +77,185 @@ void initData()
     foodMenu.add("CremeBrulee", Food10);
 }
 
-void printMainMenu() {
+void printFoodMenu()
+{
+    foodMenu.print();
+}
+
+void memberMainMenu() {
+
     cout << "+----------------------------+" << endl;
-    cout << "+     Welcome to The Bear!   +" << endl;
+    cout << "+    How may we serve you?   +" << endl;
     cout << "+----------------------------+" << endl;
     cout << "" << endl;
     cout << "+----------------------------+" << endl;
-    cout << "+   Please select an option  +" << endl;
-    cout << "+   1) Register as a Member  +" << endl;
-    cout << "+   2) Login as Admin        +" << endl;
-    cout << "+   3) Login as Member       +" << endl;
+    cout << "+    [1] Make an Order       +" << endl;
+    cout << "+    [2] Cancel an Order     +" << endl;
+    cout << "+    [3] Manage Account      +" << endl;
+    cout << "+    [4] Exit                +" << endl;
     cout << "+----------------------------+" << endl;
-}
+
+    int opt = 0;
+    while (opt != 4)
+    {
+        cout << "Please select an option" << endl;
+        cin >> opt;
+
+        if (opt == 1)
+        {
+            printFoodMenu();
+        }
+
+        else if (opt == 2)
+        {
+
+        }
+
+        else if (opt == 3)
+        {
+
+        }
+
+        else
+        {
+            cout << "Invalid Input!" << endl;
+        }
+    }
+
+    cout << "Goodbye! It was a pleasure serving you!" << endl;
 
 
-void printRegisterMemberHeader(){
-    cout << "+---------------------------------+" << endl;
-    cout << "+       Register as a Member      +" << endl;
-    cout << "+---------------------------------+" << endl;
 }
+
 void printAdminLoginHeader() {
     cout << "+---------------------------------+" << endl;
     cout << "+         Login as Admin          +" << endl;
     cout << "+---------------------------------+" << endl;
 }
 
-void printMemberLoginHeader() {
-    cout << "+---------------------------------+" << endl;
-    cout << "+        Login as Member          +" << endl;
-    cout << "+---------------------------------+" << endl;
-}
+
 
 void registerMember() {
-    string memberEmail = "";
+    cout << "+---------------------------------+" << endl;
+    cout << "+       Register as a Member      +" << endl;
+    cout << "+---------------------------------+" << endl;
+    cout << "" << endl;
+    string memberUsername = "";
     string memberPassword = "";
-    cout << "Please enter an email: " << endl;
-    cin >> memberEmail;
+    double memberMoney = 0;
+    cout << "Please enter a username: " << endl;
+    cin >> memberUsername;
 
     cout << "Please enter a password: " << endl;
     cin >> memberPassword;
+
+    cout << "Please enter how much money you would like to add to your account:" << endl;
+
+    try
+    {
+        cin >> memberMoney;
+    }
+    catch (const std::exception&)
+    {
+        cout << "Invalid Amount!" << endl;
+    }
+
+
+
+    Member newMember(memberUsername, memberPassword, memberMoney, 0);
+
+    memberHashTable.add(newMember.getName(), newMember);
+}
+
+void memberLogin() {
+    cout << "+---------------------------------+" << endl;
+    cout << "+        Login as Member          +" << endl;
+    cout << "+---------------------------------+" << endl;
+    cout << "" << endl;
+    string memberUsername = "";
+    string memberPassword = "";
+    double memberMoney = 0;
+    cout << "Please enter your username: " << endl;
+    cin >> memberUsername;
+
+    cout << "Please enter your password: " << endl;
+    cin >> memberPassword;
+
+    if (memberHashTable.checkExist(memberUsername) == true)
+    {
+        loggedInMember = memberHashTable.get(memberUsername);
+        cout << "Successful Login!" << endl;
+        memberMainMenu();
+    }
+
+    else
+    {
+        string loginOpt = "";
+        cout << "Invalid Login Credentials!" << endl;
+        cout << "Don't have an account?" << endl;
+        cout << "Enter 1 to register an account. Enter anything to return to the Login menu." << endl;
+        cin >> loginOpt;
+
+        if (loginOpt == "1")
+        {
+            registerMember();
+        }
+
+        else
+        {
+            memberLogin();
+        }
+    }
+
+}
+
+void mainMenu() {
+    cout << "+----------------------------+" << endl;
+    cout << "+     Welcome to The Bear!   +" << endl;
+    cout << "+----------------------------+" << endl;
+    cout << "" << endl;
+    cout << "+----------------------------+" << endl;
+    cout << "+   Please select an option  +" << endl;
+    cout << "+   [1] Register as a Member +" << endl;
+    cout << "+   [2] Login as Admin       +" << endl;
+    cout << "+   [3] Login as Member      +" << endl;
+    cout << "+   [4] Exit                 +" << endl;
+    cout << "+----------------------------+" << endl;
+
+    int opt = 0;
+    cin >> opt;
+
+    while (opt != 4) 
+    {
+        if (opt == 1)
+        {
+            registerMember();
+        }
+
+        else if (opt == 2)
+        {
+            printAdminLoginHeader();
+        }
+
+        else if (opt == 3)
+        {
+            memberLogin();
+        }
+
+        else
+        {
+            cout << "Please enter a valid input!" << endl;
+            mainMenu();
+        }
+    }
+
+    cout << "Goodbye! It was a pleasure serving you!" << endl;
 
 }
 
 int main(){
     initData();
-    printMainMenu();
-    int opt = 0;
-    cin >> opt;
-
-    if (opt == 1) 
-    {
-        printRegisterMemberHeader();
-        registerMember();
-    }
-
-    else if (opt == 2) 
-    {
-        printAdminLoginHeader();
-    }
-
-    else if (opt == 3) 
-    {
-        printMemberLoginHeader();
-    }
-
-    else 
-    {
-        cout << "Please enter a valid input!" << endl;
-        main();
-    }
+    //mainMenu();
+    registerMember();
 }
 
