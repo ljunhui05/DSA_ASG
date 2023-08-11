6592c3ac152365be8cc2b58e29d1c3ebabebdc6b
limjunhui <110986279+ljunhui05@users.noreply.github.com>
2023-08-11 22:44:15 +0800

    Add comments for OrderQueue

--- DSA_ASG/OrderQueue.cpp
+++ DSA_ASG/OrderQueue.cpp
@@ -3,11 +3,15 @@
 using namespace std;
 #include "OrderQueue.h";
 
+//Lim JunHui (S10242387K)
+//Description: Constructor for Queue class
 Queue::Queue() {
 	frontNode = nullptr;
 	backNode = nullptr;
 }
 
+//Lim JunHui (S10242387K) 
+//Description: Destructor for Queue class
 Queue::~Queue() {
 	Node* temp = frontNode;
 	while (temp != nullptr) {
@@ -18,6 +22,11 @@ Queue::~Queue() {
 
 }
 
+//Lim JunHui (S10242387K)
+//Input parameters: Order object as item
+//Return value: boolean value
+//Description: This function enqueues the a order object to the back of the Queue and returns true 
+//once it has successfully done so
 bool Queue::enqueue(OrderItem item) {
 	struct Node* newNode = new Node;
 	newNode->item = item;
@@ -35,6 +44,9 @@ bool Queue::enqueue(OrderItem item) {
 	return true;
 }
 
+//Lim JunHui (S10242387K)
+//Return value: boolean value
+//Description: This function dequeues the first Order object in the Queue and returns true once it has done so
 bool Queue::dequeue() {
 	if (frontNode == backNode) {
 		frontNode = nullptr;
@@ -49,6 +61,12 @@ bool Queue::dequeue() {
 	return true;
 }
 
+//Lim JunHui (S10242387K)
+//Input parameters: A dereferenced Order object as item
+//Return value: boolean value
+//Description: This function dequeues the first Order object in the Queue and returns true once it has done so.
+//This function also assigns the value of the first order object in the Queue to a dereferenced Order object
+//called item
 bool Queue::dequeue(OrderItem& item) {
 	if (frontNode == backNode) {
 		item = frontNode->item;
@@ -65,13 +83,22 @@ bool Queue::dequeue(OrderItem& item) {
 	return true;
 }
 
-
+//Lim JunHui (S10242387K)
+//Input parameters: A dereferenced Order object as item
+//Description: This function assigns the value of the first Order object in the queue to a dereferenced 
+//Order object called item 
 void Queue::getFront(OrderItem& item) {
 	if (frontNode != nullptr) {
 		item = frontNode->item;
 	}
 }
 
+//Lim JunHui (S10242387K)
+//Input parameters:Member object as member
+//Return value: Returns a list of Order objects
+//Description: This function loops through the Queue to find the Order objects that have the name of the member that
+//is the input parameter. If the names match, it is added into a list of Order objects called memberOrderList. Once
+//the function has reached the end of the Queue, it returns memberOrderList
 List<Order> Queue::getMemberOrder(Member member) {
 	List<Order> memberOrderList;
 	Node* temp = frontNode;
@@ -87,6 +114,9 @@ List<Order> Queue::getMemberOrder(Member member) {
 	return memberOrderList;
 }
 
+//Lim JunHui (S10242387K)
+//Return value: boolean value
+//Description: This function checks if the Queue is empty. It returns true if it is. Else it returns false
 bool Queue::isEmpty() {
 	if (frontNode == nullptr) {
 		return true;
@@ -97,6 +127,10 @@ bool Queue::isEmpty() {
 	}
 }
 
+//Lim JunHui (S10242387K)
+//Return value: Length of the Queue as int
+//Description: This function loops through the Queue and adds 1 to a counter called len whenever it moves to the
+//next item in the Queue. len is then returned.
 int Queue::getLength() {
 	int len = 0;
 	Node* temp = backNode;
